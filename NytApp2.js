e888fcd94fbe667a6c1790031f50bb4d637c0732
Alec Synnestvedt <aleccsyn@gmail.com>
2021-05-10 07:45:47 -0400

    push

---
+++ NytApp2.js
@@ -0,0 +1,65 @@
+import React, {useState} from 'react';
+import NytResults from './NytResults'
+
+const baseURL = 'https://api/nytimes.com/svc/search/v2/articlesearch.json';
+const key = 'PY7QGMTkKGcikCNp3KfpPnAqSi5F8uGM';
+
+const [ search, setSearch ] = useState('');
+const [ startDate, setStartDate ] = useState('');
+const [endDatge, setEndDate ] = useState('');
+const [ pageNumber, setPageNumber ] = useState(0);
+const [ results, setResults ] = useState([]);
+
+const NytApp2 = () => {
+  const fetchResults = () => {
+    let url = `${baseURL}?api-key=${key}&{pageNumber}&q=${search}`;
+    url = startDate ? url + `&begin_date=${startDate}` : url;
+    url = endDate ? url + `&end_date=${endDate}` : url;
+  
+    fetch(url)
+      .then(res => res.json())
+      .then(data => setResults(data.response.docs))
+      .catch(err => console.log(err));
+  };
+  const handleSubmit = (event) => {
+    setPageNumber(0);
+    fetchResults();
+    event.preventDefault()
+  }
+
+  const changePageNumber = (event, direction) => {
+    event.preventDefault()
+    if (direction === 'down') {
+      if (pageNumber > 0) {
+        setPageNumber(pageNumber - 1);
+        fetchResults();
+      }
+    }
+    if (direction ==='up') {
+      setPageNumber(pageNumber + 1);
+      fetchResults();
+    }
+  }
+  return(
+    <div className="main">
+      <div className="mainDiv">
+        <form onSubmit={(e) => handleSubmit(e)}>
+          <span>Enter a single search term: </span>
+          <input type="text" name="search" onChange={(e) => setSearch(e.target.value)} required />
+          <br />
+          <span>Enter a start date: </span>
+          <input type="date" name="startDate" pattern="[0-9]{8}" onChange={(e) => setStartDate(e.target.value)} />
+          <span>Enter an end date: </span>
+          <input type="date" name="endDate" pattern="[0-9]{8}" onChange={(e) => setEndDate(e.target.value)} />
+          <br />
+          <button className="submit">Submit search</button>
+        </form>
+        {
+          results.length > 0 <NytResults results={results} changePage={ChangePageNumber} /> : null
+        }
+      </div>
+    </div>
+  )
+};
+
+export default NytApp2;
